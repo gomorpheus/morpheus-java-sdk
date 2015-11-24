04a92309ba2b64265ffaed0714c5dfd392e75483
William Chu <will.chu@me.com>
2015-11-24 11:11:43 -0800

    Adding GetInstance support

---
+++ src/main/java/com/morpheus/sdk/provisioning/GetInstanceRequest.java
@@ -0,0 +1,75 @@
+package com.morpheus.sdk.provisioning;
+
+import com.morpheus.sdk.exceptions.MorpheusApiRequestException;
+import com.morpheus.sdk.internal.AbstractApiRequest;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+/**
+ * A request object for defining a request for fetching a specific instance within the Morpheus Account.
+ * Typically this object is called from the {@link com.morpheus.sdk.MorpheusClient MorpheusClient} class and
+ * is used to fetch an {@link Instance} object.
+ *
+ * Example Usage:
+ * <pre>
+ *     {@code
+ *     	MorpheusClient client = new MorpheusClient(credentialsProvider);
+ *     	GetInstanceRequest request = new GetInstanceRequest();
+ *     	GetInstanceResponse response = client.getInstance(request);
+ *     	return response.instance;
+ *     }
+ * </pre>
+ * @author William Chu
+ */
+public class GetInstanceRequest extends AbstractApiRequest<GetInstanceResponse> {
+  private Long instanceId;
+  /**
+   * Executes the request against the appliance API (Should not be called directly).
+   */
+  @Override
+  public GetInstanceResponse executeRequest() throws MorpheusApiRequestException {
+    CloseableHttpClient client = null;
+    try {
+      URIBuilder uriBuilder = new URIBuilder(endpointUrl);
+      uriBuilder.setPath("/api/instances/" + this.getInstanceId());
+      HttpGet request = new HttpGet(uriBuilder.build());
+      this.applyHeaders(request);
+      HttpClientBuilder clientBuilder = HttpClients.custom();
+      clientBuilder.setDefaultRequestConfig(this.getRequestConfig());
+      client = clientBuilder.build();
+
+      CloseableHttpResponse response = client.execute(request);
+      return GetInstanceResponse.createFromStream(response.getEntity().getContent());
+    } catch(Exception ex) {
+      //Throw custom exception
+      throw new MorpheusApiRequestException("Error Performing API Request for Listing Instances", ex);
+    } finally {
+      if(client != null) {
+        try {
+          client.close();
+        } catch(IOException io) {
+          //ignore
+        }
+      }
+    }
+  }
+
+  public Long getInstanceId() {
+    return instanceId;
+  }
+
+  public void setInstanceId(Long instanceId) {
+    this.instanceId = instanceId;
+  }
+
+  public GetInstanceRequest instanceId(Long instanceId) {
+    this.instanceId = instanceId;
+    return this;
+  }
+}
